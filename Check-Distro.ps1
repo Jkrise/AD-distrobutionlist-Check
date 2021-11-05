251c837ac7e48230f2c3702ec18c3a55df8fb04d
Jkrise <40506057+Jkrise@users.noreply.github.com>
2021-11-05 08:36:55 -0500

    Update Check-Distro.ps1
    Fixed grammar / spelling of help

--- Check-Distro.ps1
+++ Check-Distro.ps1
@@ -1,88 +1,63 @@
 <#
 .SYNOPSIS
 
-Script for comparing users in a company list to users in a distrobution list.
+Script for comparing users in a company list to users in a distribution list.
 
 DESCRIPTION
 
-This PowerShell script take a Company Name (Company) and an optional Department (Dept) name along with the name of a Distrobution list (Distro) and comparesthe active Directy information for the two entries and displays a list of differences if any are found. Thus allowing users to ensure Distrobution lists are properly maintained when new employees are added to the system. If mulitple distro checks are done against the same Company/Dept, the previously cached Comapany/Dept data will be used to increase the efficiency of the process. If a different Company or Dept is used, the User list will be refreshed automatically.
+This PowerShell script take a Company Name (Company) and an optional Department (Dept) name along with the name of a Distribution list (Distro) and compares the active Directory information for the two entries and displays a list of differences if any are found. Thus, allowing users to ensure Distribution lists are properly maintained when new employees are added to the system. If multiple distro checks are done against the same Company/Dept, the previously cached Company/Dept data will be used to increase the efficiency of the process. If a different Company or Dept is used, the User list will be refreshed automatically.
 
-A copy of the Company user list, the users in the Distrobution list as well as a list of the differences between the two lists are Automatically saved to the userd Desktop in a Sub-Folder called Contacts
+A copy of the Company user list, the users in the Distribution list as well as a list of the differences between the two lists are Automatically saved to the user’s Desktop in a Sub-Folder called Contacts
 
 In Addition, the Get-Departments function allows the user to retrieve a list of departments belonging to the selected company where needed.
 
 USAGE:
 
-Check-Distro [[-Company (Required)] <String>] [-Distro (Required)] <String> [[-Dept] <String>] [[-UpdateAD] <String>]
-or
-Get-Departments [[-Company (Required)] <String>]'
+Check-Distro [[-Company (Required)] ] [-Distro (Required)] [[-Dept] ] [[-UpdateAD] ] or Get-Departments [[-Company (Required)] ]'
 
-.PARAMETER Company
-The Company name to pul the list of EMployee names and Email addresses from in the Active Directory. This item is Required.
+.PARAMETER Company The Company name to pull the list of Employee names and Email addresses from in the Active Directory. This item is Required.
 
-.PARAMETER Distro
-Name of the Distribution List used to verify the Members of the Company is Contains.
+.PARAMETER Distro Name of the Distribution List used to verify the Members of the Company is Contains.
 
-.PARAMETER Dept
-Filters the list of Employees in the Company to a specific department. If not specified, all users from all departments will be used.
+.PARAMETER Dept Filters the list of Employees in the Company to a specific department. If not specified, all users from all departments will be used.
 
-.PARAMETER UpdateAD
-If specified, this option prompts the users to add the missing contacts to the Ditrobution list. (This function is depricated)
+.PARAMETER UpdateAD If specified, this option prompts the users to add the missing contacts to the Distribution list. (This function is deprecated)
 
-.PARAMETER Company
-For the Get-Departments function, this required command specifies which Company in the Active Directory to get the list of Departments from.
+.PARAMETER Company For the Get-Departments function, this required command specifies which Company in the Active Directory to get the list of Departments from.
 
 .EXAMPLE
 
-PS C:\> Get-Departments -Company 'Mercury'
-This command would scan the Active Directory listing for the Comapny 'Mercury' and return a list of all of the departments listed in the AD for Mercury
+PS C:> Get-Departments -Company 'Mercury' This command would scan the Active Directory listing for the Company 'Mercury' and return a list of all the departments listed in the AD for Mercury
 
-PS C:\>Get-Departments -Company 'a la mode'
-This command would scan the Active Directory listing for the Comapny 'a la mode' and return a list of all of the departments listed in the AD for a la mode
+PS C:>Get-Departments -Company 'a la mode' This command would scan the Active Directory listing for the Company 'a la mode' and return a list of all the departments listed in the AD for a la mode
 
-PS C:\> Check-Distro -Company 'a la mode' -Distro 'vsg-dl-alm-supportdept'
-This command will query the Active Directory listing for all users with the Company name of 'a la mode' and will check if the selected users exist in the Distrobution list 'vsg-dl-alm-supportdept'; If any users from 'a la mode' are missing, they will be displayed on screen.
+PS C:> Check-Distro -Company 'a la mode' -Distro 'vsg-dl-alm-supportdept' This command will query the Active Directory listing for all users with the Company name of 'a la mode' and will check if the selected users exist in the Distrobution list 'vsg-dl-alm-supportdept'; If any users from 'a la mode' are missing, they will be displayed on screen.
 
-PS C:\> Check-Distro -Company 'a la mode' -Distro 'vsg-dl-alm-supportdept' -Dept 'Customer Support'
-This command will query the Active Directory listing for all users with the Company name of 'a la mode' and will filter the list to only users listed with a Department of 'Customer Support'; Next it will check if the selected users exist in the Distrobution list 'vsg-dl-alm-supportdept'; If any users from 'a la mode' are missing, they will be displayed on screen.
+PS C:> Check-Distro -Company 'a la mode' -Distro 'vsg-dl-alm-supportdept' -Dept 'Customer Support' This command will query the Active Directory listing for all users with the Company name of 'a la mode' and will filter the list to only users listed with a Department of 'Customer Support'; Next it will check if the selected users exist in the Distrobution list 'vsg-dl-alm-supportdept'; If any users from 'a la mode' are missing, they will be displayed on screen.
 
-PS C:\> Check-Distro -Company 'a la mode' -Distro 'vsg-dl-alm-supportdept' -Dept 'Customer Support' -UpdateAD 'True'
-This function will perform the same process as the example above; however, it will additionally prompt the user to add the missing contacts to the Distrobution list. (Currently this function is disabled)
+PS C:> Check-Distro -Company 'a la mode' -Distro 'vsg-dl-alm-supportdept' -Dept 'Customer Support' -UpdateAD 'True' This function will perform the same process as the example above; however, it will additionally prompt the user to add the missing contacts to the Distrobution list. (Currently this function is disabled)
 
+PS C:> Check-Distro -Company 'a la mode' -Distro 'CVS-DL-OKCCampus' This command will query the Active Directory listing for all users with the Company name of 'a la mode' and will check if the selected users exist in the Distrobution list 'CVS-DL-OKCCampus'; If any users from 'a la mode' are missing, they will be displayed on screen.
 
-PS C:\> Check-Distro -Company 'a la mode' -Distro 'CVS-DL-OKCCampus'
-This command will query the Active Directory listing for all users with the Company name of 'a la mode' and will check if the selected users exist in the Distrobution list 'CVS-DL-OKCCampus'; If any users from 'a la mode' are missing, they will be displayed on screen.
+PS C:> Check-Distro -Company 'a la mode' -Distro 'CVS-DL-ALM-Everyone' This command will query the Active Directory listing for all users with the Company name of 'a la mode' and will check if the selected users exist in the Distrobution list 'CVS-DL-ALM-Everyone'; If any users from 'a la mode' are missing, they will be displayed on screen.
 
-PS C:\> Check-Distro -Company 'a la mode' -Distro 'CVS-DL-ALM-Everyone'
-This command will query the Active Directory listing for all users with the Company name of 'a la mode' and will check if the selected users exist in the Distrobution list 'CVS-DL-ALM-Everyone'; If any users from 'a la mode' are missing, they will be displayed on screen.
-
-PS C:\> Check-Distro -Company 'TSG' -Distro 'CVS-DL-ALM-Everyone' -Dept 'ALM*'
-This command will query the Active Directory listing for all users with the Company name of 'TSG' and a department of ALM* (so anything that starts with ALM like 'ALM Testing' will match and check if the selected users exist in the Distrobution list 'CVS-DL-ALM-Everyone'; If any users from 'a la mode' are missing, they will be displayed on screen.
-
+PS C:> Check-Distro -Company 'TSG' -Distro 'CVS-DL-ALM-Everyone' -Dept 'ALM*' This command will query the Active Directory listing for all users with the Company name of 'TSG' and a department of ALM* (so anything that starts with ALM like 'ALM Testing' will match and check if the selected users exist in the Distrobution list 'CVS-DL-ALM-Everyone'; If any users from 'a la mode' are missing, they will be displayed on screen.
 
 .INPUTS
 
-For Check-Distro :
-	Company - This is the name of the Company filter to get the list of users from. (I.E. 'a la mode', 'Mercury' or 'TSG')
-	Distro - The name of the Distrobution list to comapre against (This is a reguired field). (I.E. 'vsg-dl-alm-supportdept', CVS-DL-OKCCampus' or  'CVS-DL-ALM-Everyone')
-	Dept - This Option field applies a secondary filter to the company list narrowing it down from the whole company to a specific department within the company. (I.E. 'Customer Support', 'Mercury Sales' or 'Marketing-Nzd')
-	UpdateAD - This optional feature enables the prompt to update the Distrobution List with any missing users. Presently while the prompt can be enabed, the function itsef is currently not avaialable.
+For Check-Distro : Company - This is the name of the Company filter to get the list of users from. (I.E. 'a la mode', 'Mercury' or 'TSG') Distro - The name of the Distribution list to compare against (This is a required field). (I.E. 'Distribution-dl-alm-supportdept', 'CVS-DL-OKCCampus' or 'CVS-DL-ALM-Everyone') Dept - This Option field applies a secondary filter to the company list narrowing it down from the whole company to a specific department within the company. (I.E. 'Customer Support', 'Mercury Sales' or 'Marketing-Nzd') UpdateAD - This optional feature enables the prompt to update the Distrobution List with any missing users. Presently while the prompt can be enabled, the function itself is currently not available.
 
-For Get-Departments:
-	Company - This required field is used to search the Active Directory listing and returns a list of unique departments for the selected company
+For Get-Departments: Company - This required field is used to search the Active Directory listing and returns a list of unique departments for the selected company
 
 .OUTPUTS
 
-For Check-Distro :
-	A list of users missing from the Distro (if any are found) are disapled on the screen in Red
-	A csv list of the Company users (and Department is used) is saved to the users desktop in a folder called 'Contacts'
-	A CSV list of the Uers from the Distrobution List is saved to the users desktop in a folder called 'Contacts'
-	A CSV list of the difference in users, if any, is saved to the users desktop in a folder called 'Contacts'
-	
-For Get-Departments:
-	A list of the departments is displaey on screen
-	
+For Check-Distro : A list of users missing from the Distro (if any are found) are disabled on the screen in Red A csv list of the Company users (and Department is used) is saved to the user’s desktop in a folder called 'Contacts' A CSV list of the Users from the Distrobution List is saved to the user’s desktop in a folder called 'Contacts' A CSV list of the difference in users, if any, is saved to the user’s desktop in a folder called 'Contacts'
+
+For Get-Departments: A list of the departments is displayed on screen
+
 .NOTES
+
+
 
 	This Powershell script and its functions were written by Jason Krise and utilizes information found online in thefollowing references:
 	https://shellgeek.com/powershell-get-list-of-users-in-ad-group/
@@ -282,8 +257,8 @@ Check-Distro -Company 'Mercury' -Distro 'CVS-DL-ALM-MERC-Everyone'
 # SIG # Begin signature block
 # MIIjPgYJKoZIhvcNAQcCoIIjLzCCIysCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUMrVIT2UUqyRtF2mjCvSF3ydE
-# qP6ggh3WMIIE/jCCA+agAwIBAgIQDUJK4L46iP9gQCHOFADw3TANBgkqhkiG9w0B
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUQPr04w8Jf9U5SVcuBVZKDLiK
+# 9iOggh3WMIIE/jCCA+agAwIBAgIQDUJK4L46iP9gQCHOFADw3TANBgkqhkiG9w0B
 # AQsFADByMQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYD
 # VQQLExB3d3cuZGlnaWNlcnQuY29tMTEwLwYDVQQDEyhEaWdpQ2VydCBTSEEyIEFz
 # c3VyZWQgSUQgVGltZXN0YW1waW5nIENBMB4XDTIxMDEwMTAwMDAwMFoXDTMxMDEw
@@ -447,26 +422,26 @@ Check-Distro -Company 'Mercury' -Distro 'CVS-DL-ALM-MERC-Everyone'
 # Q29kZSBTaWduaW5nIFJTQTQwOTYgU0hBMzg0IDIwMjEgQ0ExAhAK+FP5bpXTvx4s
 # HxE0Euw+MAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3AgEMMQowCKACgAChAoAAMBkG
 # CSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwGCisGAQQBgjcCAQsxDjAMBgorBgEE
-# AYI3AgEVMCMGCSqGSIb3DQEJBDEWBBRWTV11IgLtUVA9hMsFUCERFuxH1TANBgkq
-# hkiG9w0BAQEFAASCAYCkyDaggR29DwZcIPqrD/0oR8uX6GgUjHA+Bu5Yj6mv+ZMP
-# 4mtN7KofpYltvNL7cm/fo3ZiqLergvAzhvoJpwO7zoT42Ilt75GEh3pt8jR73Mot
-# iiXjwE2n0fwfqzmzXxzNb1UUfS4Jeu1Cn1kjahkHZeFseyhoWFghAipW+UM8GaAo
-# lK8BweZsZMl8jn8w4fHDFiBbsBJvnJyiJNuXENMXzGirdx6brnm8c41dByyzELay
-# Cx+sHBCHpS9WI0Gcj6HWcYZkDiHq5wPj0M0otkCdfDxcccErx2bhXesdBgxqh+qS
-# kD61l5zY9NNN+FfnOeHiARSzFq+84iFLuFOUgp7WVv98SAhSbl6R9Mbm92rucuU4
-# t5WhCm5gwt8VKG01VKn6BjAmH4T11VALedD285hx8hNFBn7k/OZnYuildrXhnJyU
-# z9FZFFJB6Z1OP3I6nalYpk+jMj5g5F6MbJ060vrqx5aSvEBG+fTjgVHWUCO13Jo0
-# rrI1z4kImHswRmhb0buhggIwMIICLAYJKoZIhvcNAQkGMYICHTCCAhkCAQEwgYYw
+# AYI3AgEVMCMGCSqGSIb3DQEJBDEWBBQRVtJgg9UvdRJ+4TfBgJ9SOAGZczANBgkq
+# hkiG9w0BAQEFAASCAYABNcX/LTL/bFvTB4FLwIT+fhWo/cSf+j/M8XeNCk95K+XW
+# WInuWqavSV/+0PE6KEdDaxO1mSdXZ8Vkv2t8Amu7jMLWFsXepFwsNDao8zl6qBFD
+# MbeXktvNIDdDyl+eMbaneyNTvtTysukJcsPuRGFnIKUxSu2XrWN9/jtr+SBi6gS4
+# F9wjydKnM6577bZ7XAx/LTg7puJsFaFXQM/5U62Zf7iO/981+P1dfZQ/TOyMCS31
+# 0W0jaPQrzi6bwdsTwPdGw+w+r9xSh3QtWdL2Zdo5r91ldOaD+NLBAscsbeCCiWoh
+# nDoNfKlAhsZ2d05JrWTXwVfxDA+CbxEqdok8eebgEGdrwRNz2XMaFhZbt2y8T6xE
+# qy3ro8rZuI+pPOGiX36J6Dg9x9hmF3Mj46NsFEq7hiYtU1fNCFdBPH1Yc4hi9D24
+# xHcmi9N6RqmmTSLcapxad/e1eS7nX1tPLOaaJ6hT6Y8JJKDL/wbmrYATpXlfQRzs
+# ay45xx02tVnG1shU/46hggIwMIICLAYJKoZIhvcNAQkGMYICHTCCAhkCAQEwgYYw
 # cjELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQ
 # d3d3LmRpZ2ljZXJ0LmNvbTExMC8GA1UEAxMoRGlnaUNlcnQgU0hBMiBBc3N1cmVk
 # IElEIFRpbWVzdGFtcGluZyBDQQIQDUJK4L46iP9gQCHOFADw3TANBglghkgBZQME
 # AgEFAKBpMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8X
-# DTIxMTEwNDE2MjYxMVowLwYJKoZIhvcNAQkEMSIEIMBL8IDh2OwhG4PYcbE63rmO
-# 99at0lyR8OyK8EfoRypZMA0GCSqGSIb3DQEBAQUABIIBAHfS8M9Igxss3ZKz02WD
-# T8ZGhhEaPqYlfcwGZmhQBOZliWFW7Jdu5GDemOCYDDJk+lPYFYsdN1blZmprgAAc
-# Qn/KkiafjxlTK7P7RvtXhKV0tUDywuzmCTlyzPbEyR60vnO0scCXtEZdxolPcZAg
-# iId0GPXkJaYVKg1zLCIHFoBRWuzbyqn6bg+4rgzDDgSI4awPPJXLDOTtqUkStmVk
-# CjOjKFoIeZ2vxuo4KfTW4DI39lMHhQGoA3Dn1Lhd0PCpwqQTCVnPjw8UTiSylQdH
-# jjPnS1dZxxeNrXZrgDlfoSVhAaHJk2HqhRhQX71x43KpRWhIg6GVmBTGR5Gr/D+C
-# I/I=
+# DTIxMTEwNTEzMzUzNlowLwYJKoZIhvcNAQkEMSIEIOaZWMbRlvt+SsjbNWAqp2IU
+# VgOD+aFKuqtM4lsCE/daMA0GCSqGSIb3DQEBAQUABIIBAAH5sFNcDLEC4PBxdWc4
+# WhWbH4DcBynGRE0+KBeRQZA63AKRrtJJHOjsGm+0pdk4UtcmfONbiSwp3g601aj1
+# aSZgeInrSbquiU1iBOuuNXmBJOGtW7KWc9ADMnjm+DIo/TLxePzlrteWsEdpz7Up
+# UkY1kKgMR6LJ7I8P1Ys9M1P+lY3zDTIYDkCeWxZ+uR0KvUvKEaiOHd1lc+dVGFLv
+# w+0QOg+VnRLcBcpaR1e3bTGlttCScvMjKpnGYOTG/lBR6w6H8fh1qEE5K7IxjldQ
+# XwRqy26t65uLmhr+z1uVQ4IE/kHhb6GRyg/uaMRoYBfIyywHOGAB/uaGTekCdOud
+# 6+o=
 # SIG # End signature block
